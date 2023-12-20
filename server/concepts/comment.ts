ee37a1d527d0ee046c60a362321ddd5c490b6089
Ege Kabasakaloglu <egekabas@mit.edu>
2023-12-20 02:19:21 -0500

    Comments

--- server/concepts/comment.ts
+++ server/concepts/comment.ts
@@ -1,21 +1,22 @@
 import { Filter, ObjectId } from "mongodb";
-import DocCollection from "../framework/doc";
-import { PostDoc } from "./post";
+import DocCollection, { BaseDoc } from "../framework/doc";
 
-export interface CommentDoc<Content> extends PostDoc<Content> {
+export interface CommentDoc<CommentContent> extends BaseDoc {
+  author: ObjectId;
+  content: CommentContent;
   originalPost: ObjectId;
 }
 
-export interface CommentConcept<Content> {
-  createComment(author: ObjectId, content: Content, originalPost: ObjectId): Promise<ObjectId>;
+export interface CommentConcept<CommentContent> {
+  createComment(author: ObjectId, content: CommentContent, originalPost: ObjectId): Promise<ObjectId>;
   deleteComment(_id: ObjectId): Promise<void>;
-  getComments(query: Filter<CommentDoc<Content>>): Promise<CommentDoc<Content>[]>;
-  getComment(_id: ObjectId): Promise<CommentDoc<Content>>;
+  getComments(query: Filter<CommentDoc<CommentContent>>): Promise<CommentDoc<CommentContent>[]>;
+  getComment(_id: ObjectId): Promise<CommentDoc<CommentContent>>;
 }
 
-export class BasicCommentConcept<Content> implements CommentConcept<Content> {
-  public readonly comments = new DocCollection<CommentDoc<Content>>("posts");
-  async createComment(author: ObjectId, content: Content, originalPost: ObjectId): Promise<ObjectId> {
+export class BasicCommentConcept<CommentContent> implements CommentConcept<CommentContent> {
+  public readonly comments = new DocCollection<CommentDoc<CommentContent>>("posts");
+  async createComment(author: ObjectId, content: CommentContent, originalPost: ObjectId): Promise<ObjectId> {
     const _id = await this.comments.createOne({ author, content, originalPost });
     return _id;
   }
@@ -25,13 +26,13 @@ export class BasicCommentConcept<Content> implements CommentConcept<Content> {
     }
     await this.comments.deleteOne({ _id });
   }
-  async getComments(query: Filter<CommentDoc<Content>>): Promise<CommentDoc<Content>[]> {
+  async getComments(query: Filter<CommentDoc<CommentContent>>): Promise<CommentDoc<CommentContent>[]> {
     const comments = await this.comments.readMany(query, {
       sort: { dateUpdated: -1 },
     });
     return comments;
   }
-  async getComment(_id: ObjectId): Promise<CommentDoc<Content>> {
+  async getComment(_id: ObjectId): Promise<CommentDoc<CommentContent>> {
     const comment = await this.comments.readOne({ _id });
     if (comment == null) {
       throw new Error("Comment not found!");
